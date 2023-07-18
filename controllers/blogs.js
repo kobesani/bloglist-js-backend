1dbc9db135d4413310f1c2783d9807e0627ecd13
kobii <harold.rockata@gmail.com>
2023-07-18 17:49:48 +0200

    Fixed error in ordering app.use calls that preventing forwarding req body, added get, delete by id

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -11,13 +11,21 @@ blogsRouter.get("/", (request, response) => {
     });
 });
 
-// blogsRouter.get("/:id", (request, response) => {
-//   Blog
-//     .findById(request.params.id)
-//     .then(blogs => {
-//       response.json(blogs);
-//     });
-// });
+blogsRouter.get("/:id", (request, response) => {
+  Blog
+    .findById(request.params.id)
+    .then(foundBlog => {
+      response.json(foundBlog);
+    });
+});
+
+blogsRouter.delete("/:id", (request, response) => {
+  Blog
+    .findByIdAndDelete(request.params.id)
+    .then(deletedBlog => {
+      response.status(204).json(deletedBlog);
+    });
+});
 
 blogsRouter.post("/", (request, response) => {
   logger.info(request.json);
--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const blogsRouter = require("./controllers/blogs");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 
-app.use("/api/blogs", blogsRouter);
 app.use(cors());
 app.use(express.json());
+app.use("/api/blogs", blogsRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
