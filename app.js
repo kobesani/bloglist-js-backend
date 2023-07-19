9f871ee9811cdbacc34cecfb2e51044d5a917eca
kobii <harold.rockata@gmail.com>
2023-07-19 10:16:15 +0200

    Adding morgan logging + basic error handling to app

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require("express");
 const cors = require("cors");
 
 const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.morganConfig);
 app.use("/api/blogs", blogsRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
