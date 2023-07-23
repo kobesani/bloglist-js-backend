f8dc9dee9378c9aabe21d66479c36780fabd35a0
kobii <harold.rockata@gmail.com>
2023-07-24 00:08:01 +0200

    Added get and delete specific blogs tests

--- tests/bloglist_api.test.js
+++ tests/bloglist_api.test.js
@@ -70,6 +70,33 @@ test("a blog with no title cannot be added", async () => {
   expect(retrievedBlogs).toHaveLength(helper.initialBlogs.length);
 });
 
+test("a specific blog can be viewed", async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToView = blogsAtStart[0];
+
+  const resultBlog = await api
+    .get(`/api/blogs/${blogToView.id}`)
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  expect(resultBlog.body).toEqual(blogToView);
+});
+
+test("a blog can be deleted", async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToDelete = blogsAtStart[0];
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(200);
+
+  const blogsAtEnd = await helper.blogsInDb();
+
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+  const titles = blogsAtEnd.map(blog => blog.title);
+  expect(titles).not.toContain(blogToDelete.title);
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
