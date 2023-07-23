623b17664028879c41e1a68e438cf098fa075f85
kobii <harold.rockata@gmail.com>
2023-07-23 23:22:01 +0200

    Added validator to blop schema to prevent being able to add entries without required info

--- models/blog.js
+++ models/blog.js
@@ -14,10 +14,10 @@ mongoose
 
 const blogSchema = new mongoose.Schema(
   {
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    url: { type: String, required: true },
+    likes: { type: Number, required: true }
   }
 );
 
