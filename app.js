50f40fcb78735cddd6f188404c31ade7d9fcb223
kobii <harold.rockata@gmail.com>
2023-07-25 15:47:12 +0200

    Adding new users controller to app

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const config = require("./utils/config");
 const logger = require("./utils/logger");
 
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 const middleware = require("./utils/middleware");
 
 mongoose.set("strictQuery", false);
@@ -21,7 +22,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(middleware.morganConfig);
+
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
+
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
