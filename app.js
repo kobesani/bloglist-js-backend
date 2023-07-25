d997333b74ed617fdc35596631f5d264e8a3d19e
kobii <harold.rockata@gmail.com>
2023-07-25 15:27:38 +0200

    move mongoose mongodb config and connection spin up to app.js

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
 const blogsRouter = require("./controllers/blogs");
 const middleware = require("./utils/middleware");
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  // eslint-disable-next-line no-unused-vars
+  .then(result => logger.info(`Connected to Mongodb: ${config.DATABASE_NAME}`))
+  // eslint-disable-next-line no-unused-vars
+  .catch(error => logger.error("Error connecting to MongoDB"));
 
 const app = express();
 
