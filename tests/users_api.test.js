725bf2aa6dc3aaabc3d9a72fce1abe84959b2806
kobii <harold.rockata@gmail.com>
2023-07-25 16:52:52 +0200

    Added initial test setup and one test for users to create a new one

---
+++ tests/users_api.test.js
@@ -0,0 +1,51 @@
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const config = require("../utils/config");
+const User = require("../models/user");
+const helper = require("./test_helper");
+
+const api = supertest(app);
+
+beforeAll(config.createMongoDbConnection);
+
+describe("when there is initially one user in the db", () => {
+  beforeEach(async () => {
+    await User.deleteMany();
+
+    const passwordHash = await bcrypt.hash("password123", 10);
+    const user = new User({
+      username: "root",
+      name: "root",
+      passwordHash
+    });
+
+    await user.save();
+  });
+
+  test("creation succeeds with a fresh username", async () => {
+    const usersAtStart = await helper.usersInDb();
+    const newUser = {
+      username: "kobesani",
+      name: "Kobe Rockata",
+      password: "password456"
+    };
+
+    await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map(user => user.username);
+    expect(usernames).toContain(newUser.username);
+  });
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+});
