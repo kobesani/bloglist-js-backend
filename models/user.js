98a3a1281ec5e0256d3738e0f991a225804b65a0
kobii <harold.rockata@gmail.com>
2023-07-25 15:33:12 +0200

    Adding user schema and model

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      minlength: 3
+    },
+    name: {
+      type: String,
+      required: true,
+      minlength: 3
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    }
+  }
+);
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    // don't reveal the password hash
+    delete returnedObject.passwordHash;
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
